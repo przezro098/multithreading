23dfe4529ed7d93e823883ebe1de2f12348e41a3
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-05-25 12:06:51 +0200

    Test refactorization

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,17 +3,29 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class RadarTest {
 
-    @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    private Scud scud;
+    private PatriotBattery patriotBattery;
+    private int missiles = 0;
+
+    @BeforeEach public void initialization() {
+        patriotBattery = mock(PatriotBattery.class);
+        scud = new Scud();
+
+    }
+
+    @RepeatedTest(value = 20) public void checkIfBatteryWillLaunchSinglePatriotOnSingleEnemyMissileTest() {
+        missiles = 1;
+        PatriotLauncher launchPatriotic = new PatriotLauncher(missiles);
+        BetterRadar radar = new BetterRadar(patriotBattery, launchPatriotic, missiles);
+        radar.notice(scud);
+        verify(patriotBattery).launchPatriot(scud);
     }
 
 }
